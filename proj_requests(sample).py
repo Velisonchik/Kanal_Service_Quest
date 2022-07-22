84c5faca4e2c1ebdfc93ab96e03c80297589454e
velisonchik <velisonchik@gmail.com>
2022-07-22 19:07:49 +0300

    Add sample proj_request

---
+++ proj_requests(sample).py
@@ -0,0 +1,7 @@
+google_file_ID = '11N5RSv321A5t2131n-IAZdA3211230HaFfo_cQ'
+sheet_file = '.\\test.txt'
+user_db = "postgres"
+passwd_db = "1234567896"
+host_db = "127.0.0.1"
+port_db = "5432"
+database_db = "postgres"
